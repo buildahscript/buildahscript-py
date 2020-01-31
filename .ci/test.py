a60bbec3736a7d884cc937f8130b2ecbf60bc9d0
Jamie Bliss <jamie@ivyleav.es>
2020-01-31 16:40:39 -0500

    Rewrite for Python2

---
+++ .ci/test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python2
+import os
+import subprocess
+
+subprocess.check_call(['docker', 'build', '-t', 'tester', '.ci/Dockerfile'])
+
+for script in os.listdir('demos'):
+    subprocess.check_call([
+        'docker', 'run', '--rm', '--init', '--privileged', 'tester', script,
+    ])
--- .ci/test.xsh
+++
@@ -1,4 +0,0 @@
-docker build -t tester .ci/Dockerfile
-
-for script in g`demos/*`
-    docker run --init --privileged tester @(script)
